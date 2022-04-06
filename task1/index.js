cb9e9bec242e56d54b40676a53df4ede48813281
Anton Zhuravlov <zhuravlov1999@gmail.com>
2022-04-06 15:54:55 +0300

    do dots calculator

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,26 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(str) {
+  const elementsArr = str.split(' ')
+
+  const firstOperand = elementsArr[0];
+  const secondOperand = elementsArr[elementsArr.length - 1];
+  const operator = elementsArr[1];
+
+  console.log(elementsArr)
+  switch (operator) {
+    case '+':
+      console.log(elementsArr)
+      return firstOperand + secondOperand
+      break;
+    case '-':
+      return firstOperand.slice(0, (firstOperand.length -  secondOperand.length))
+      break;
+    case '*':
+      return firstOperand.repeat(secondOperand.length)
+      break;
+    case '//':
+      return firstOperand.slice(0, (firstOperand.length / secondOperand.length)) || ''
+      break;
+  }
+  
 }
+console.log(calculate(".. - .."))
