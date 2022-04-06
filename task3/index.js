1d77ffef94f86ad895b12c5b5193f1c57dd4f053
Anton Zhuravlov <zhuravlov1999@gmail.com>
2022-04-06 15:57:22 +0300

    do parentheses validator

--- task3/index.js
+++ task3/index.js
@@ -1,3 +1,15 @@
-function isValidParentheses() {
-  //code
+function isValidParentheses(str) {
+  if (str.length <= 0 || str.length > 100) return false;
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let char = stack[stack.length - 1]
+    if(str[i] === '('){
+      stack.push(str[i])
+    } else if (char === '(' && str[i] === ')'){
+      stack.pop()
+    } else return false
+  }
+  return stack.length ? false : true
 }
+
+console.log(isValidParentheses("("))
